ecd239acde862398325ab47e8c12b6771785da24
Jaylo <jaylogalam04@gmail.com>
2025-09-18 23:31:57 +0800

    save

--- client/src/feautures/books/dependency.ts
+++ client/src/feautures/books/dependency.ts
@@ -1,5 +1,8 @@
 import { BookPersistence } from "./data/bookPersistence";
 import { GetAllBooks } from "./domain/bookUsecase";
+abstract class BookDependencies {
+    public abstract boo
+}
 
 class BookDependency {
   bookRepository = new BookPersistence();
