fd28425f2c7b269e8d09fad0dbf4090ac1b4bea5
Jaylo <jaylogalam04@gmail.com>
2025-09-19 21:49:00 +0800

    minor changes

--- client/src/feautures/books/domain/bookRepository.ts
+++ client/src/feautures/books/domain/bookRepository.ts
@@ -1,7 +1,7 @@
 import { BookEntity } from "./bookEntity.ts";
 
 export abstract class BookRepository {
-  public abstract getAllBooks(): Promise<BookEntity[]>;
+  abstract getAllBooks(): Promise<BookEntity[]>;
   abstract checkoutBook(book: BookEntity): Promise<void>;
   abstract returnBook(book: BookEntity): Promise<void>;
 }
